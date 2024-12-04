2c38bbfb405d80ca07de9a70bdfd868a6352d0be
Ballylight <ballylight153@gmail.com>
2024-12-04 17:13:26 +0100

    jhqeuhhwd

---
+++ app/components/Sections/CTA/Cta.js
@@ -0,0 +1,19 @@
+import styles from './cta.module.css';
+
+
+{/* Footer Section */}
+<section className={styles.footer}>
+<div className={styles.footerContainer}>
+    <div className={styles.footerText}>
+        <h2 className={styles.footerHeading}>Celebrating Our Achievements</h2>
+        <p className={styles.footerDescription}>
+            While we take pride in our accomplishments, our greatest reward is witnessing our clients reach
+            their health goals and thrive. Become a part of PNg Ftfam Solutions today and begin your own
+            journey toward a healthier, happier you.
+        </p>
+    </div>
+    <div className={styles.footerButtonContainer}>
+        <button className={styles.contactButton}>Contact Us</button>
+    </div>
+</div>
+</section>
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 // import About from '@/components/Section/About/About';
 // import Analytic from '@/components/Section/Analytic/Analytic';
 // import Choose from '@/components/Section/Choose/Choose';
-// import Faq from '@/components/Section/FAQ/Faq';
+ import Cta from './components/Section/Cta/Cta';
 // import Hero from '@/components/Section/Hero/Hero';
 // import Plan from '@/components/Section/Plan/Plan';
 // import Testimonial from '@/components/Section/Testimonial/Testimonial';
@@ -14,8 +14,8 @@ export default function Home() {
      <Choose />
      <Analytic />
      <Plan />
-     <Testimonial />
-     <Faq /> */}
+     <Testimonial />*/}
+     <Cta />
     </div>
   );
 }
